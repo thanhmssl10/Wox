385e41be1b1ea8946f0fdde1f15f487bda52cd49
Jeremy Wu <jeremy24wu@gmail.com>
2020-03-02 07:42:20 +1100

    Remove check

--- SolutionAssemblyInfo.cs
+++ SolutionAssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]<====
+[assembly: AssemblyFileVersion("1.2.0")]
 [assembly: AssemblyInformationalVersion("1.2.0")]
